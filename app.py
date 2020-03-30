6ea866e137f55d861906dd850baf6fb50bfb1dac
benm4ckenzie <b_m@me.com>
2020-03-30 20:54:57 +0000

    added task input and selector

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
