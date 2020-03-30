d2af67ba0cdadc542e68ae6b9b8bd398bd74cfc3
benm4ckenzie <b_m@me.com>
2020-03-30 14:40:01 +0000

    added parent child template

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task manager'
+app.config["MONGO_URI"] = 'mongodb+srv://benm4ckenzie:r00tUser@myfirstcluster-beoum.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return'Hello world.... again!'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
