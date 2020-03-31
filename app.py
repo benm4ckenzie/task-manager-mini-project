a45fba39af062670c93c2a987533f9bd597e6dc6
benm4ckenzie <b_m@me.com>
2020-03-31 16:24:38 +0000

    added submit button as well as push new task to mongo

--- app.py
+++ app.py
@@ -17,7 +17,14 @@ def get_tasks():
 
 @app.route('/add_task')
 def add_task():
-    return render_template("addtask.html")
+    return render_template("addtask.html",
+    categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
