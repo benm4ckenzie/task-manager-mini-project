072c5d45a494f05934738972189a7c61e6886fec
benm4ckenzie <b_m@me.com>
2020-04-02 19:49:12 +0000

    added categories page

--- app.py
+++ app.py
@@ -49,6 +49,10 @@ def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
 
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', categories=mongo.db.categories.find())
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
