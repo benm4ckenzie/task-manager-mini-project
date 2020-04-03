f6509bb29aeba69d8d4ed8f7cbdcfd7978279bd0
benm4ckenzie <b_m@me.com>
2020-04-03 15:02:09 +0000

    added insert category

--- app.py
+++ app.py
@@ -15,10 +15,12 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+
 @app.route('/add_task')
 def add_task():
     return render_template("addtask.html",
     categories=mongo.db.categories.find())
+
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
@@ -26,11 +28,13 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task, categories=all_categories)
+
 
 @app.route('/update_task/<task_id>', methods=['POST'])
 def update_task(task_id):
@@ -44,14 +48,17 @@ def update_task(task_id):
     })
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html', categories=mongo.db.categories.find())
+
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
@@ -66,6 +73,28 @@ def update_category(category_id):
         {'_id': ObjectId(category_id)},
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    categories=mongo.db.categories
+    category_doc = {'category_name': request.form.get('category_name')}
+    categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
+
+
+    
     
 
 if __name__ == '__main__':
